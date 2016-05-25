21f262a4bf6c8417d4a7540c0a939fcb57a97169
Dan <dvfranklin@gmail.com>
2016-05-25 16:10:57 -0400

    Added context

--- src/main/java/com/theironyard/SpringCrudApplication.java
+++ src/main/java/com/theironyard/SpringCrudApplication.java
@@ -2,11 +2,12 @@ package com.theironyard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringCrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCrudApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringCrudApplication.class, args);
 	}
 }
